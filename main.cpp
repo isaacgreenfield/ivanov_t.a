e1a1925de6c76be5934308eb955fac37f054ea00
isaacgreenfield <ivtim708@gmail.com>
2025-11-10 12:57:22 +0300

    try/catch created

--- main.cpp
+++ main.cpp
@@ -6,28 +6,32 @@ struct IntArray
   int get(size_t id) const;
   size_t size() const;
   int last() const;
+  IntArray(int i);
+  ~IntArray();
 };
 
 int main()
 {
   int next = 0;
   std::cin >> next;
-  IntArray a;
-  a.add(next);
-  while (std::cin >> next)
-  {
-    a.add(next);
-  }
-  if (std::cin.fail()) {
-    return 1;
-  }
+  try {
+    IntArray a(next);
+    while (std::cin >> next)
+    {
+      a.add(next);
+    }
+    if (std::cin.fail()) {
+      return 1;
+    }
 
-  size_t count = 1;
-  for (size_t i = 0; i < a.size() - 1; ++i)
-  {
-    int d = a.get(i);
-    count+=!(d%a.last());
-  }
-  std::cout << count << std::endl;
+    size_t count = 1;
+    for (size_t i = 0; i < a.size() - 1; ++i)
+    {
+      int d = a.get(i);
+      count+=!(d%a.last());
+    }
+    std::cout << count << std::endl;
+  } catch (...) {
+    return 2;
   return 0;
 }
